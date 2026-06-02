696a1533b48d8706700a48a0d07802580fcdbb7c
MURILOCASAGRANDE <MURILOCASAGRANDE@TQR224232>
2026-06-01 21:07:00 -0300

    Mudança de Nome do banco eu acho...

---
+++ WebCRUDMVCSQL/Migrations/20260602000444_MudancaDoNome.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebCRUDMVCSQL.Migrations
+{
+    public partial class MudancaDoNome : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
